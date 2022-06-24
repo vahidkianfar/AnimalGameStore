9a2f35ad48297c168ebe118c4ed376ce205f6058
vahidkianfar <vahidkianfar@gmail.com>
2022-06-24 13:35:07 +0100

    Added test for Retrieving Specific Fossil Name.

--- GameTests/TDD_Start.cs
+++ GameTests/TDD_Start.cs
@@ -1,5 +1,12 @@
 using AnimalGameStore.HttpServices;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using FluentAssertions;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
 namespace GameTests;
 
 public class Tests
@@ -20,7 +27,26 @@ public class Tests
     {
         var client = new HttpClient();
         var response = client.GetAsync($"https://acnhapi.com/v1/fossils/amber");
-        //Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result);
-        Assert.IsNotNull(response);
+        Assert.That(response, Is.Not.Null);
+    }
+    [Test]
+    public void Http_GET_Convert_Result_To_Json_Object()
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync($"https://acnhapi.com/v1/fossils/amber");
+        var json = response.Result.Content.ReadAsStringAsync().Result;
+        var fossil = JsonNode.Parse(json);
+        fossil.Should().BeOfType<JsonObject>();
+    }
+    [Test]
+    public void Retrieve_Fossil_Name_From_Converted_JSON_Object()
+    {
+        var client = new HttpClient();
+        var fossilName="amber";
+        var response = client.GetAsync($"https://acnhapi.com/v1/fossils/{fossilName}");
+        var json = response.Result.Content.ReadAsStringAsync().Result;
+        var fossil = JsonNode.Parse(json);
+
+        Assert.That(fossil["file-name"]!.ToString(), Is.EqualTo(fossilName));
     }
 }
